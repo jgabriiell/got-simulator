7c9ca77f66de70cd57538dd3e8eccadfecc64107
João Gabriel <jgabriiell33@gmail.com>
2024-07-30 12:20:23 -0300

    Importing Project

---
+++ src/test/java/me/dio/got/GotApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.got;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
